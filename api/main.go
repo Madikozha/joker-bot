52e6b4e7a3e07d9017b9a3884d386df81177e3e7
missayev <feeerhd.342@gmail.com>
2024-12-01 01:58:28 +0500

    refactor: main.go package to handler 2

--- api/main.go
+++ api/main.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -8,69 +9,66 @@ import (
 	"strings"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
-	"github.com/joho/godotenv"
 )
 
-func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Start")
-	err := godotenv.Load()
-	if err != nil {
-		log.Println(err.Error(), err)
-	}
+var bot *tgbotapi.BotAPI
+
+func init() {
 	bot, err := tgbotapi.NewBotAPI(os.Getenv("TG_TOKEN"))
 	if err != nil {
-		log.Println(err.Error(), err)
+		log.Fatalf("Error initializing bot: %v", err)
 	}
 
 	bot.Debug = true
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+}
+func Handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Start")
 
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
-
-	updates, err := bot.GetUpdatesChan(u)
-	if err != nil {
-		log.Println(err.Error(), err)
+	var update tgbotapi.Update
+	if err := json.NewDecoder(r.Body).Decode(&update); err != nil {
+		log.Println("Failed to decode update:", err)
+		return
 	}
-	for update := range updates {
-		tgChannelID := update.Message.Chat.ID
-		if update.Message != nil {
-			firstName := update.Message.From.FirstName
-			lastName := update.Message.From.LastName
-			useRespond := strings.ToLower(update.Message.Text)
+	if update.Message == nil {
+		return
+	}
+	tgChatID := update.Message.Chat.ID
+	firstName := update.Message.From.FirstName
+	lastName := update.Message.From.LastName
+	useRespond := strings.ToLower(update.Message.Text)
 
-			if firstName == "ToTa" && lastName == "TatO" && (strings.HasPrefix(useRespond, "hi joker") || strings.HasSuffix(useRespond, "hi joker")) {
-				log.Printf("[%s] %s\n", update.Message.From.UserName, update.Message.Text)
+	if firstName == "ToTa" && lastName == "TatO" && (strings.HasPrefix(useRespond, "hi joker") || strings.HasSuffix(useRespond, "hi joker")) {
+		log.Printf("[%s] %s\n", update.Message.From.UserName, update.Message.Text)
 
-				gif := gifHandler(tgChannelID, "https://i.imgur.com/Kd3hMX6.mp4", "Hi master!")
+		gif := gifHandler(tgChatID, "https://i.imgur.com/Kd3hMX6.mp4", "Hi master!")
 
-				bot.Send(gif)
-				fmt.Println("Sending")
+		bot.Send(gif)
+		fmt.Println("Sending")
 
-			} else if firstName != "ToTa" && lastName != "TatO" && (strings.HasPrefix(useRespond, "hi joker") || strings.HasSuffix(useRespond, "hi joker")) {
-				log.Printf("[%s] %s\n", update.Message.From.UserName, update.Message.Text)
+	} else if firstName != "ToTa" && lastName != "TatO" && (strings.HasPrefix(useRespond, "hi joker") || strings.HasSuffix(useRespond, "hi joker")) {
+		log.Printf("[%s] %s\n", update.Message.From.UserName, update.Message.Text)
 
-				gif := gifHandler(tgChannelID, "https://i.pinimg.com/originals/9f/80/73/9f807378cd83071ca8ea09e05dd03cdc.gif", "Who are you?")
+		gif := gifHandler(tgChatID, "https://i.pinimg.com/originals/9f/80/73/9f807378cd83071ca8ea09e05dd03cdc.gif", "Who are you?")
 
-				bot.Send(gif)
-				fmt.Println("Sending")
-			}
+		bot.Send(gif)
+		fmt.Println("Sending")
 
-		}
 	}
 }
 
-func main() {
-	http.HandleFunc("/", Handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
-func gifHandler(tgChannelID int64, urlStr, caption string) *tgbotapi.AnimationConfig {
+func gifHandler(tgChatID int64, urlStr, caption string) *tgbotapi.AnimationConfig {
 	gif := tgbotapi.NewAnimationShare(
-		tgChannelID,
+		tgChatID,
 		urlStr,
 	)
 
 	gif.Caption = caption
 
 	return &gif
+}
+
+func main() {
+	http.HandleFunc("/", Handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
